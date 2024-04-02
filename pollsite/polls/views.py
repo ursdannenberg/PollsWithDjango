1a444679236fc5a3ecb739eb9e02482c1ddfdcd1
ursdannenberg <ur.schaefer@live.de>
2024-04-02 16:59:06 +0200

    Added Python linter.

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse #, Http404
 #from django.template import loader
-from django.shortcuts import render, get_object_or_404
 
 from .models import Question
+
+# Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]

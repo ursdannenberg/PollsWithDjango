a461e58b164d73df0c588c81cdacaba5d4a5a404
ursdannenberg <ur.schaefer@live.de>
2024-04-02 12:52:32 +0200

    Added more views.

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -5,3 +5,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world! You're at the index of polls.")
+
+def detail(request, question_id):
+    response = "You're looking at question %s."
+    return HttpResponse(response % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    response = "You're voting on question %s."
+    return HttpResponse(response % question_id)
+

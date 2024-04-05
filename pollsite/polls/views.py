62536ba817e9f08f4813d67855806b73bbebfb3b
ursdannenberg <ur.schaefer@live.de>
2024-04-05 09:11:30 +0200

    IndexView no longer displays future questions. Introduced tests for IndexView.

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect #, HttpResponse, Http404
 #from django.template import loader
@@ -20,9 +22,12 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 #def detail(request, question_id):
 #    #try:
